e89d1c146a7da74eabe8717d89cf20406ba09020
riverjoy <tosakarinmylove@gmail.com>
2022-10-29 21:10:53 +0800

    recordList存入localstorage

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -55,6 +55,7 @@ export default class NumberPad extends Vue{
     }
     ok(){
       this.$emit('update:value',parseFloat(this.output))
+      this.$emit('submit')
     }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,9 +1,10 @@
 <template>
   <Layout class-prefix="layout">
-  <NumberPad :value.sync="record.amount"></NumberPad>
+    {{recordList}}
+  <NumberPad :value.sync="record.amount" @submit="saveRecord"></NumberPad>
   <Types :value.sync="record.type"></Types>
   <Notes @update:value="onUpdateNotes"></Notes>
-  <Tags :data-source.sync="tags" @update:value="onUpdateTags"></Tags>
+  <Tags :data-source.sync="tags" @update:value="onUpdateTags" ></Tags>
     {{record}}
   </Layout>
 </template>
@@ -15,6 +16,9 @@ import Types from '@/components/Money/Types.vue';
 import Tags from '@/components/Money/Tags.vue';
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+
+window.localStorage.setItem('recordVersion','0.0.1')
+const recordList:Record[] =JSON.parse(window.localStorage.getItem('recordList') || "[]")
 
 type Record = {
   type:string,
@@ -29,11 +33,19 @@ type Record = {
 export default class Money extends Vue {
   tags= ['衣','食','住','行'];
   record:Record = {type:'-',amount:0,tags:[],notes:''};
+  recordList:Record[] = recordList;
   onUpdateNotes(value:string){
     this.record.notes =value
   }
   onUpdateTags(value:string[]){
     this.record.tags = value
+  }
+  saveRecord(){
+    const newRecord =JSON.parse(JSON.stringify(this.record))
+    console.log(newRecord);
+    recordList.push(newRecord)
+    console.log(recordList);
+    window.localStorage.setItem('recordList',JSON.stringify(recordList))
   }
 }
 </script>
