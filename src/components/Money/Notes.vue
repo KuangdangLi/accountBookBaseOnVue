7e340fc1f91cf033b1ae41fc72b5c8d12cd4a599
riverjoy <tosakarinmylove@gmail.com>
2022-10-29 15:32:31 +0800

    Notes组件增加最大输入长度“10”

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,7 +2,7 @@
   <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" placeholder="在这里输入备注" v-model="value">
+      <input type="text" placeholder="在这里输入备注" maxlength="10" v-model="value" >
     </label>
     {{value}}
   </div>
