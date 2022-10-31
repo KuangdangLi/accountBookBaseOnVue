e07cef84fc41ce9710fb59999c332dc510b7765e
riverjoy <tosakarinmylove@gmail.com>
2022-10-31 17:25:11 +0800

    完成EditLabel组件

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,85 @@
+<template>
+  <layout>
+  <div class="navBar">
+    <Icon class="leftIcon" name="left" @click.native="goBack"></Icon>
+    <span class="title">编辑标签</span>
+    <span class="rightIcon"></span>
+  </div>
+  <div class="form-wrapper">
+    <FormItem :value="tag.name"
+              @update:value="update"
+              title="标签名" edit-place-holder="请输入标签名"/>
+  </div>
+  <div class="button-wrapper">
+    <Button @click="remove">删除标签</Button>
+  </div>
+  </layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Button from '@/components/Button.vue';
+import FormItem from "@/components/FormItem.vue"
+import tagListModel from '@/models/tagListModel';
+
+const tagList = tagListModel.fetch()
+
+@Component({components: {Button,FormItem}})
+export default class EditLabel extends Vue {
+  tag: Tag | undefined
+  created(){
+    const id = this.$route.params.id
+    const index = tagList.map(tag=>tag.id).indexOf(id);
+    if(index>=0){
+      this.tag = tagList[index]
+    }else{
+      this.$router.replace('/404')
+    }
+  }
+  goBack(){
+    this.$router.back()
+  }
+  update(name:string){
+    if(this.tag) tagListModel.update(this.tag.id,name)
+  }
+  remove(){
+    if(this.tag) {
+      tagListModel.remove(this.tag.id);
+      window.alert('删除成功')
+      this.$router.back()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.navBar {
+  text-align: center;
+  font-size: 16px;
+  padding: 12px 16px;
+  background: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  > .title {
+  }
+  > .leftIcon {
+    width: 24px;
+    height: 24px;
+  }
+  > .rightIcon {
+    width: 24px;
+    height: 24px;
+  }
+}
+.form-wrapper {
+  background: white;
+  margin-top: 8px;
+}
+.button-wrapper {
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16px;
+}
+</style>
