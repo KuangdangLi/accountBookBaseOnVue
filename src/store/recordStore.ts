556b89c804400fb50d86a9687611e6a3ce9b5227
riverjoy <tosakarinmylove@gmail.com>
2022-11-01 13:15:52 +0800

    model模块重构成全局数据管理store模块后删除。

---
+++ src/store/recordStore.ts
@@ -0,0 +1,24 @@
+import clone from "@/lib/clone.ts"
+
+const localStorageKeyName = 'recordList';
+
+const recordStore = {
+  recordList:[] as RecordItem[],
+  fetchRecord():RecordItem[]{
+    this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]")
+    return this.recordList
+  },
+  saveRecord(){
+    window.localStorage.setItem(localStorageKeyName,JSON.stringify(this.recordList))
+  },
+  createRecord(data:RecordItem){
+    data.createdAt = new Date();
+    // const newRecord = clone(data);
+    this.recordList.push(clone(data))
+    this.saveRecord()
+  }
+}
+
+recordStore.fetchRecord()
+
+export default recordStore;
