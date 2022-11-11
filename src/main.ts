587d8150e23ffb90a530eeaef4638182b0d040d2
riverjoy <tosakarinmylove@gmail.com>
2022-11-11 22:44:13 +0800

    添加pc设备浏览页面出现二维码的功能

--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,15 @@ window.onload = function () {
     window.scrollTo(0, 200);
   }, 0);
 };
+
+if(document.documentElement.clientWidth >500 ){
+  window.alert('为了您得到满意的浏览效果，请用手机扫描二维码打开本页面')
+  const img = document.createElement('img')
+  img.src = '/qrCode.png'
+  img.style.position = 'fixed'
+  img.style.left = '50%'
+  img.style.top = '50%'
+  img.style.transform = 'translate(-50%,-50%)'
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)'
+  document.body.appendChild(img)
+}
