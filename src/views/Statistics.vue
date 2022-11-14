f5ec996e94273ff670c9b70e7a5d28d9f4d045e9
riverjoy <tosakarinmylove@gmail.com>
2022-11-14 14:37:06 +0800

    引入chart组件

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,7 +2,9 @@
   <div>
     <layout>
         <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="value"></Tabs>
-
+        <div class="chart-wrapper" ref="chartWrapper">
+          <MyChart class="chart" :options="options"></MyChart>
+        </div>
         <ul v-if="groupedList.length>0">
           <li v-for="group in groupedList" :key="group.title">
            <h3 class="title">{{beautifyDate(group.title)}}<span>￥{{group.total}}</span></h3>
@@ -29,11 +31,13 @@ import {mixins} from 'vue-class-component';
 import stateHelper from '@/mixins/stateHelper';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
+import MyChart from '@/components/MyChart.vue';
+import {EChartOption} from 'echarts';
 
 type Result =  {title:string,items:RecordItem[],total?:number}[]
 
 @Component({
-  components: {Tabs}
+  components: {MyChart, Tabs}
 })
 export default class Statistics extends mixins(stateHelper) {
   get recordList() {
@@ -84,6 +88,116 @@ export default class Statistics extends mixins(stateHelper) {
     })
       return result
   }
+  get options():EChartOption|undefined{
+    if(this.dateAmountList){
+      return {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        grid: {
+          left: 0,
+          right: 0,
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: this.dateAmountList['dateList'],
+            axisTick: {
+              alignWithLabel: true
+            },
+            axisLabel:{
+              formatter(value:string){
+                return value.substr(5)
+              }
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: 'Direct',
+            type: 'bar',
+            barWidth: '30%',
+            data: this.dateAmountList['amountList']
+          }
+        ]
+      }
+    }else{
+      return {
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        grid: {
+          left: 0,
+          right: 0,
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['暂无数据','暂无数据'],
+            axisTick: {
+              alignWithLabel: true
+            },
+            axisLabel:{
+              formatter(value:string){
+                return value
+              }
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: 'Direct',
+            type: 'bar',
+            barWidth: '30%',
+            data: [0,0]
+          }
+        ]
+      }
+    }
+  }
+  get dateAmountList(){
+    console.log('this.groupedList');
+    console.log(this.groupedList);
+    if(this.groupedList[0]){
+      const lastDay = dayjs(this.groupedList[0].title)
+      let dateArray = []
+      let amountArray = []
+      let correspondingDateItem
+      for(let i=0;i<=30;i++){
+        let dayItem = lastDay.subtract(i,'day').format('YYYY-MM-DD')
+        dateArray.push(dayItem)
+        correspondingDateItem = this.groupedList.filter(item=>item.title === dayItem)[0];
+        amountArray.push( correspondingDateItem ? correspondingDateItem.total : 0)
+        // console.log(lastDay.subtract(i,'day').format('MM月DD'));
+      }
+      console.log(dateArray);
+      console.log(amountArray);
+      return {dateList:dateArray,amountList:amountArray}
+    }else{
+      return undefined
+    }
+
+  }
   recordTypeList = recordTypeList
   value = '-'
 }
@@ -123,5 +237,14 @@ export default class Statistics extends mixins(stateHelper) {
     margin-right: auto;
     margin-left: 16px;
     color: #999;
+  }
+  .chart{
+    width: 430%;
+    &-wrapper{
+      overflow: auto;
+      &::-webkit-scrollbar{
+        display: none;
+      }
+    }
   }
 </style>
