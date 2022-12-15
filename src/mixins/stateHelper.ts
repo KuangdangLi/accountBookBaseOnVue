0b588231c3f36ac13ef3b33be72df1ca4a1076a5
riverjoy <tosakarinmylove@gmail.com>
2022-12-15 19:44:45 +0800

    stateHelper.ts删除createTag方法，Labels页面tag展示、方法适应Tag类型定义变化

--- src/mixins/stateHelper.ts
+++ src/mixins/stateHelper.ts
@@ -2,21 +2,16 @@ import Vue from 'vue'
 import Component from 'vue-class-component';
 import store from '@/store';
 
-const errorMap:{[key:string]:string} = {
-  'duplicate Tag name' : '标签名重复',
-  'the tag name is too long': '标签名过长',
-}
+// const errorMap:{[key:string]:string} = {
+//   'duplicate Tag name' : '标签名重复',
+//   'the tag name is too long': '标签名过长',
+// }
 
 @Component
 export class stateHelper extends Vue {
   beforeCreate(){
     store.commit('fetchTags')
     store.commit('fetchRecords')
-  }
-  createTag(){
-    const name = window.prompt('请输入标签名')
-    store.commit('createTag',name)
-    if(store.state.createTagError){window.alert(errorMap[store.state.createTagError.message])}
   }
 }
 
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,5 +1,6 @@
 <template>
     <layout>
+      <Tabs :data-source="recordTypeList" class-prefix="type" :value.sync="type"></Tabs>
       <div class="tags">
         <router-link class="tag" :to="`/labels/edit/${tag.id}`" v-for="tag in tags" :key="tag.id">
           <span>{{ tag.name }}</span>
@@ -14,18 +15,28 @@
 
 <script lang="ts">
 import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
 import Button from '@/components/Button.vue';
 import store from '@/store/index';
 import {mixins} from 'vue-class-component';
 import stateHelper from '@/mixins/stateHelper';
+import RecordTypeList from '@/constants/recordTypeList';
 
 
 @Component({
-  components: {Button}
+  components: {Tabs, Button}
 })
 export default class Labels extends mixins(stateHelper) {
-  tags = store.state.tagList
-
+  type:Type = '-'
+  get tags(){
+    return  store.state.tagList.filter(tag => tag.type === this.type);
+  }
+  recordTypeList = RecordTypeList
+  createTag(){
+    const name = window.prompt('请输入标签名')
+    store.commit('createTag', {name,type:this.type})
+    if(store.state.createTagError){window.alert(store.state.createTagError.message)}
+  }
 }
 </script>
 
