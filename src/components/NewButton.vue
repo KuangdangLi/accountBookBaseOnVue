e69e785b562967bd6c12942bca96bfc3031a2da4
riverjoy <tosakarinmylove@gmail.com>
2022-12-16 16:57:11 +0800

    新增组件NewButton

---
+++ src/components/NewButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="button" @click="add">
+    <div class="iconWrapper">
+      <Icon :name="iconName" />
+    </div>
+    <span v-if="content">{{ content }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import {Prop} from 'vue-property-decorator';
+@Component
+export default class NewButton extends Vue {
+ @Prop() iconName!:string
+ @Prop() content!:string
+  add(){
+   this.$emit('createTag')
+  }
+}
+</script>
