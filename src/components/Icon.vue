dad632074000dedce644fdb5d8fed37d6a88aa69
riverjoy <tosakarinmylove@gmail.com>
2022-12-16 15:54:14 +0800

    Icon组件接受的外部属性变化

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,11 +1,14 @@
 <template>
     <svg class="icon">
-      <use :href="'#'+name" >
+      <use :href="'#'+svgHref()" >
       </use>
     </svg>
 </template>
 
 <script lang="ts">
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
 let importAll = (requireContext:__WebpackModuleApi.RequireContext)=> requireContext.keys().forEach(requireContext);
 try {
 importAll(require.context('../assets/icons',true,/\.svg$/));
@@ -13,17 +16,35 @@ importAll(require.context('../assets/icons',true,/\.svg$/));
   console.log(error);
 }
 
-export default  {
-  name:'Icon',
-  props: ['name']
+type IconTable = {[id:string]:string}
+
+const iconTable:IconTable = {
+  '1' : 'food',
+  '2' : 'traffic',
+  '3' : 'shopping',
+  '4' : 'electric',
+  '5' : 'salary',
+  '6' : 'bonus',
+  '7' : 'hongbao',
+  '9999': 'accountBook'
+}
+
+@Component
+export default class Icon extends Vue{
+  @Prop(String) name!:string;
+  @Prop(String) id!:string;
+  svgHref(){
+    if(this.name){
+      return this.name
+    }else if(this.id){
+      if(Number(this.id) - 7 > 0){
+        return iconTable['9999']
+      }else{
+        return ('1234567'.indexOf(this.id)+1) && iconTable[this.id]
+      }
+    }
+  }
+  // props: ['name']
 }
 </script>
 
-<style lang="scss" scoped>
-.icon {
-  width: 32px; height: 32px;
-  vertical-align: -0.15em;
-  fill: currentColor;
-  overflow: hidden;
-}
-</style>
